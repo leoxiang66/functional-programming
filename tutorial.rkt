2e10df4b869d2e4073dafbbbf42fdb6e9d0bf2c5
Tao Xiang <xiang.tao@outlook.de>
2022-11-29 13:08:21 +0100

    1

--- tutorial.rkt
+++ tutorial.rkt
@@ -1,3 +1,5 @@
+#lang lazy
+
 (define (square x) (* x x))
 
 (define (add x y) (+ x y))
@@ -5,3 +7,14 @@
 (define (sum-of-squares x y)
   (+ (square x)
      (square y)))
+
+(define greet
+  (lambda (given [surname "Smith"])
+    (string-append "Hello, " given " " surname)))
+
+(define greet2
+  (lambda x "hello"))
+
+(define addl
+  (lambda (a b) (+ a b))
+  )
